2b56d977893dfd5271ae12373a5a4e0e8ef85f3e
Himank Yadav <him229@gmail.com>
2016-02-12 23:42:35 -0600

    Business Cell outlets done

---
+++ BusinessCell.swift
@@ -0,0 +1,30 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Himank Yadav on 2/12/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
