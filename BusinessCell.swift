387b7ed3415b3fdd680a38209093fec1bbe2a070
Himank Yadav <him229@gmail.com>
2016-02-14 01:05:40 -0600

    images rounded

--- BusinessCell.swift
+++ BusinessCell.swift
@@ -35,6 +35,8 @@ class BusinessCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
